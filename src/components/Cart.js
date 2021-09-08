bfa0e0e00f905f67e37d3f07555107d231c6c283
carolinaLobos <caro.cigna@gmail.com>
2021-09-07 22:44:26 -0300

    incluyo total del carrito

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,28 +1,32 @@
-//import React, { useState } from "react";
+import React, { useState } from "react";
 import { useCartContext } from "../context/CartContext";
 import "./componentsStyles/cartStyle.css";
 
 const Cart = function(){
     const {cart, removeItem} = useCartContext();
-    
+    let acum=0;
     function eliminar(id){
         removeItem(id)
     }
     return(<div>
         <table className="tabla">
-            <th>Id</th>
+            <th>Valor</th>
             <th>Foto</th>
             <th>Nombre</th>
             <th>Cantidad</th>
-            <th>Valor</th>
+            <th>total item</th>
             <th>Eliminar</th>
-            {cart.map((prod) => {return (<tr><td>{prod.id}</td>
+            {cart.map((prod) => {
+                acum=(prod.quantity*prod.id)+acum;
+                return (
+                <tr><td>{prod.id}</td>
                 <td><img src="{prod.avatar_url}" alt="img"/></td>        
                 <td>{prod.url}</td>
                 <td>{prod.quantity}</td>
-                <td>{prod.id}</td>
+                <td>{prod.id*prod.quantity }</td>
                 <td> <button className="btnEliminar"  type="button" onClick={() => eliminar(prod.id)}>Eliminar</button></td>
                 </tr>)})}
+                <tr><td> </td><td> </td><td> </td><td> </td><td>TOTAL COMPRA</td><td>{acum}</td></tr>
         </table>
     </div>)
 }
